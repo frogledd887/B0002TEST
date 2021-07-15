8dcd381f2d4312af5f96e6b7a00f2b7e39e4569f
Liu <dorts.j2010@gmail.com>
2021-07-15 11:05:14 +0800

    build on web

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -8,7 +8,7 @@ using System.Web.Http.Cors;
 namespace B0002.Controllers
 {
     /// <summary>
-    /// 工作代號清單
+    /// 汽機車停車表單
     /// </summary>
 
     [EnableCors(origins: "*", headers: "*", methods: "*")]
@@ -24,7 +24,7 @@ namespace B0002.Controllers
 
 
         /// <summary>
-        /// 工程處訓練業務承辨人
+        /// 停車員工資料查詢
         /// </summary>
         /// <param name="id">employee id</param>
         /// <returns></returns>
@@ -46,7 +46,29 @@ namespace B0002.Controllers
             }
         }
 
+        /// <summary>
+        /// 停車員工資料寫入
+        /// </summary>
+        /// <param name="id">employee id</param>
+        /// <param name="name">employee name</param>
+        /// <param name="number">employee id</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Car/Insert")]
+        public IHttpActionResult InsertData(string id,string name,string number)
+        {
+            using (var cn = sqlserver_conn.CreateConnection("DVDBSV01"))
+            {
+                string sql = @"INSERT INTO car_user(user_id
+                ,user_name,car_no)
+                 VALUES(@id,@name,@number)
+                ";
 
+                var result = cn.Query(sql, new { id, name, number });
+                return Json(JArray.FromObject(result));
+
+            }
+        }
 
 
     }
